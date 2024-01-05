da74af1d95eaa1d86d11b3b57d6a3bda5d566033
Javier <javierlogronoaguirre@gmail.com>
2024-01-05 19:45:52 +0100

    add book

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,54 @@
 'use strict'
 
 const express = require('express')
-
 const langCtrl = require('../controllers/all/lang')
-
 const supportCtrl = require('../controllers/all/support')
-
+const openAIserviceCtrl = require('../services/openai')
+const bookCtrl = require('../services/book')
+const cors = require('cors');
 
 const api = express.Router()
+const config= require('../config')
+const myApiKey = config.Server_Key;
+const whitelist = config.allowedOrigins;
+
+  // Middleware personalizado para CORS
+  function corsWithOptions(req, res, next) {
+    const corsOptions = {
+      origin: function (origin, callback) {
+        if (whitelist.includes(origin)) {
+          callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+      },
+    };
+  
+    cors(corsOptions)(req, res, next);
+  }
+
+  const checkApiKey = (req, res, next) => {
+    // Permitir explícitamente solicitudes de tipo OPTIONS para el "preflight" de CORS
+    if (req.method === 'OPTIONS') {
+      return next();
+    } else {
+      const apiKey = req.get('x-api-key');
+      if (apiKey && apiKey === myApiKey) {
+        return next();
+      } else {
+        return res.status(401).json({ error: 'API Key no válida o ausente' });
+      }
+    }
+  };
 
 // lang routes, using the controller lang, this controller has methods
 api.get('/langs/',  langCtrl.getLangs)
 
+//services OPENAI
+api.post('/callopenai', corsWithOptions, checkApiKey, openAIserviceCtrl.callOpenAi)
+api.post('/callbook', corsWithOptions, checkApiKey, bookCtrl.callBook)
 
 //Support
-api.post('/homesupport/', supportCtrl.sendMsgLogoutSupport)
+api.post('/homesupport/', corsWithOptions, checkApiKey, supportCtrl.sendMsgLogoutSupport)
 
 module.exports = api
